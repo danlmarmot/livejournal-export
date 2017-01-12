65279e4287741f51f69d4d3ef8b41f0bc87fe7d6
dmckean <danlmarmot@gmail.com>
2017-01-12 13:27:19 -0800

    Merging code into a single file for easier refactoring

--- export.py
+++ export.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import sys
 import json
 import os
 import re
@@ -8,14 +9,45 @@ import markdown
 from bs4 import BeautifulSoup
 from datetime import datetime
 from operator import itemgetter
-from download_posts import download_posts
-from download_comments import download_comments
+import xml.etree.ElementTree as ET
+from hashlib import md5
+import requests
+
+
+# User settings
+LJ_SERVER = ""
+USERNAME = ""
+PASSWORD = ""
+YEARS = range(1989, 1989)  # first to (last + 1)
+
+# Other constants
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 8.1; rv:10.0) Gecko/20100101 Firefox/10.0'
+}
 
 TAG = re.compile(r'\[!\[(.*?)\]\(http:\/\/utx.ambience.ru\/img\/.*?\)\]\(.*?\)')
 USER = re.compile(r'<lj user="?(.*?)"?>')
 TAGLESS_NEWLINES = re.compile('(?<!>)\n')
 NEWLINES = re.compile('(\s*\n){3,}')
 SLUGS = {}
+
+
+def main():
+    if False:
+        download_posts()
+        download_comments()
+
+    # Generate the all.json files from downloaded posts and comments
+    if True:
+        pass
+
+    if False:
+        with open('posts-json/all.json', 'r') as f:
+            all_posts = json.load(f)
+        with open('comments-json/all.json', 'r') as f:
+            all_comments = json.load(f)
+
+        combine(all_posts, all_comments)
 
 
 def fix_user_links(json_dict):
@@ -185,15 +217,202 @@ def combine(posts, comments):
         save_as_markdown(post_id, subfolder, json_post, post_comments_html)
 
 
-if __name__ == '__main__':
-    if True:
-        all_posts = download_posts()
-        all_comments = download_comments()
+# Downloads for posts
 
+def fetch_month_posts(year, month):
+    response = requests.post(
+        'http://www.livejournal.com/export_do.bml',
+        headers=get_headers(),
+        cookies=get_cookies(),
+        data={
+            'what': 'journal',
+            'year': year,
+            'month': '{0:02d}'.format(month),
+            'format': 'xml',
+            'header': 'on',
+            'encid': '2',
+            'field_itemid': 'on',
+            'field_eventtime': 'on',
+            'field_logtime': 'on',
+            'field_subject': 'on',
+            'field_event': 'on',
+            'field_security': 'on',
+            'field_allowmask': 'on',
+            'field_currents': 'on'
+        }
+    )
+
+    return response.text
+
+
+def xml_to_json(xml):
+    def f(field):
+        return xml.find(field).text
+
+    return {
+        'id': f('itemid'),
+        'date': f('logtime'),
+        'subject': f('subject') or '',
+        'body': f('event'),
+        'eventtime': f('eventtime'),
+        'security': f('security'),
+        'allowmask': f('allowmask'),
+        'current_music': f('current_music'),
+        'current_mood': f('current_mood')
+    }
+
+
+def download_posts():
+    os.makedirs('posts-xml', exist_ok=True)
+    os.makedirs('posts-json', exist_ok=True)
+
+    xml_posts = []
+    for year in YEARS:
+        for month in range(1, 13):
+            print("Fetching for " + str(month) + ", " + str(year))
+            xml = fetch_month_posts(year, month)
+            xml_posts.extend(list(ET.fromstring(xml).iter('entry')))
+
+            with open('posts-xml/{0}-{1:02d}.xml'.format(year, month), 'w+') as file:
+                file.write(xml)
+
+    json_posts = list(map(xml_to_json, xml_posts))
+    with open('posts-json/all.json', 'w') as f:
+        f.write(json.dumps(json_posts, ensure_ascii=False, indent=2))
+
+    return json_posts
+
+
+# Comments
+def fetch_xml(params):
+    response = requests.get(
+        'http://www.livejournal.com/export_comments.bml',
+        params=params,
+        headers=get_headers(),
+        cookies=get_cookies()
+    )
+
+    return response.text
+
+
+def get_users_map(xml):
+    users = {}
+
+    for user in xml.iter('usermap'):
+        users[user.attrib['id']] = user.attrib['user']
+
+    with open('comments-json/usermap.json', 'w') as f:
+        f.write(json.dumps(users, ensure_ascii=False, indent=2))
+
+    return users
+
+
+def get_comment_property(name, comment_xml, comment):
+    if name in comment_xml.attrib:
+        comment[name] = int(comment_xml.attrib[name])
+
+
+def get_comment_element(name, comment_xml, comment):
+    elements = comment_xml.findall(name)
+    if len(elements) > 0:
+        comment[name] = elements[0].text
+
+
+def get_more_comments(start_id, users):
+    comments = []
+    local_max_id = -1
+
+    xml = fetch_xml({'get': 'comment_body', 'startid': start_id})
+    with open('comments-xml/comment_body-{0}.xml'.format(start_id), 'w') as f:
+        f.write(xml)
+
+    for comment_xml in ET.fromstring(xml).iter('comment'):
+        comment = {
+            'jitemid': int(comment_xml.attrib['jitemid']),
+            'id': int(comment_xml.attrib['id']),
+            'children': []
+        }
+        get_comment_property('parentid', comment_xml, comment)
+        get_comment_property('posterid', comment_xml, comment)
+        get_comment_element('date', comment_xml, comment)
+        get_comment_element('subject', comment_xml, comment)
+        get_comment_element('body', comment_xml, comment)
+
+        if 'state' in comment_xml.attrib:
+            comment['state'] = comment_xml.attrib['state']
+
+        if 'posterid' in comment:
+            comment['author'] = users.get(str(comment['posterid']), "deleted-user")
+
+        local_max_id = max(local_max_id, comment['id'])
+        comments.append(comment)
+
+    return local_max_id, comments
+
+
+def download_comments():
+    os.makedirs('comments-xml', exist_ok=True)
+    os.makedirs('comments-json', exist_ok=True)
+
+    metadata_xml = fetch_xml({'get': 'comment_meta', 'startid': 0})
+    with open('comments-xml/comment_meta.xml', 'w') as f:
+        f.write(metadata_xml)
+
+    metadata = ET.fromstring(metadata_xml)
+    users = get_users_map(metadata)
+
+    all_comments = []
+    start_id = -1
+    max_id = int(metadata.find('maxid').text)
+    while start_id < max_id:
+        start_id, comments = get_more_comments(start_id + 1, users)
+        all_comments.extend(comments)
+
+    with open('comments-json/all.json', 'w') as f:
+        f.write(json.dumps(all_comments, ensure_ascii=False, indent=2))
+
+    return all_comments
+
+
+# Authentication
+def get_cookies():
+    r1 = requests.post(LJ_SERVER + "/interface/flat", data={'mode': 'getchallenge'})
+    r1_flat = flatresponse(r1.text)
+    challenge = r1_flat['challenge']
+
+    r2 = requests.post(LJ_SERVER + "/interface/flat",
+                       data={'mode': 'sessiongenerate',
+                             'user': USERNAME,
+                             'auth_method': 'challenge',
+                             'auth_challenge': challenge,
+                             'auth_response': make_md5_from_challenge(challenge)
+                             }
+                       )
+
+    r2_flat = flatresponse(r2.text)
+
+    if r2_flat.get('ljsession', False):
+        return {'ljsession': r2_flat['ljsession']}
     else:
-        with open('posts-json/all.json', 'r') as f:
-            all_posts = json.load(f)
-        with open('comments-json/all.json', 'r') as f:
-            all_comments = json.load(f)
+        print("Did not get ljsession cookie.  Exiting")
+        sys.exit(1)
 
-    combine(all_posts, all_comments)
+
+def get_headers():
+    return HEADERS
+
+
+def flatresponse(response):
+    items = response.strip('\n').split('\n')
+    flat_response = {items[i]: items[i + 1] for i in range(0, len(items), 2)}
+    return flat_response
+
+
+def make_md5_from_challenge(challenge):
+    first_encoded = challenge + md5(PASSWORD.encode('utf-8')).hexdigest()
+    full_encoded = md5(first_encoded.encode('utf-8')).hexdigest()
+    return full_encoded
+
+
+if __name__ == '__main__':
+    main()
